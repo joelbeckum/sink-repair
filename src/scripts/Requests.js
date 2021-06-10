dfd984f89288d3e3914a37f11093adb6e5e52c5a
Joel Beckum <joelbeckum@gmail.com>
2021-06-10 12:50:57 -0400

    Initial commit - experimenting with fetch functionality in JavaScript

---
+++ src/scripts/Requests.js
@@ -0,0 +1,19 @@
+import { getRequests } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${
+                requests.map(request => {
+                    return `<li>
+                        ${request.description}
+                    </li>`
+                })
+            }
+        </ul>
+    `
+
+    return html
+}
