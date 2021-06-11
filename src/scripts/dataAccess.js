71a93573d3e478f5c6dcefc09fab7299d32a1e53
Joel Beckum <joelbeckum@gmail.com>
2021-06-11 12:54:29 -0400

    Added delete functionality to service requests

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -38,3 +38,12 @@ export const sendRequest = (userServiceRequest) => {
         })
 }
 
+export const deleteRequest = (id) => {
+    return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
